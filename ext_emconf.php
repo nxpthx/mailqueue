5654d5ef77bf59b373b583898f93e37cb88bc992
Steffen Ritter <info@rs-websystems.de>
2014-05-05 19:45:49 +0200

    [TASK] Initialize extension

---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 Mail queue',
+	'description' => 'Queue for asynchronous email processing.',
+	'category' => 'misc',
+	'author' => 'Steffen Ritter',
+	'author_email' => 'steffen.ritter@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '[rs]websystems',
+	'version' => '1.0.0-dev',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
